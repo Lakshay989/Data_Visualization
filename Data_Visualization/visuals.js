798a5cfccbee4bdc46181bac3bbef9c1ae50098f
MikeTJohnson <mtjzdd@gmail.com>
2023-07-31 11:57:28 -0600

    got d3 working

--- Data_Visualization/visuals.js
+++ Data_Visualization/visuals.js
@@ -7,26 +7,39 @@ document.addEventListener('DOMContentLoaded', () => {
         const rows = data.split('\n');
         const headers = rows[0].split(',');
         console.log(headers);
-  
+
         // Replace 'desired_column_name' with the header of the column you want to extract
-        const desiredColumnIndex = headers.indexOf('Name');
-        if (desiredColumnIndex === -1) {
-          console.error('Column not found in CSV data.');
-          return;
-        }
-  
-        const gameListElement = document.getElementById('gameList');
-        let extractedData = '';
-  
+        // const desiredColumnIndex = headers.indexOf('Name');
+        // if (desiredColumnIndex === -1) {
+        //   console.error('Column not found in CSV data.');
+        //   return;
+        // }
+
+        // const gameListElement = document.getElementById('gameList');
+        // let extractedData = '';
+
+        // for (let i = 1; i < rows.length; i++) {
+        //   const values = rows[i].split(',');
+        //   if (values.length > desiredColumnIndex) {
+        //     extractedData += values[desiredColumnIndex];
+        //   }
+        // }
+
+          let gameListData = [];
         for (let i = 1; i < rows.length; i++) {
-          const values = rows[i].split(',');
-          if (values.length > desiredColumnIndex) {
-            extractedData += values[desiredColumnIndex] + '<br>';
-          }
+            const values = rows[i].split(',');
+            gameListData[i-1] = values[0];
         }
-  
-        gameListElement.innerHTML = extractedData;
+
+        //gameListElement.innerHTML = extractedData;
+          let gameList = d3.select("div1");
+        gameList.selectAll('p').data(gameListData).join(
+            enter => {enter.append('p').text((d, i) => d)}
+        );
       })
       .catch(error => console.error('Error fetching the CSV file:', error));
   });
+
+//changes the color of the game name when the game is selected
+
   
