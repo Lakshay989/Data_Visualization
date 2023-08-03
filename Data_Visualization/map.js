a049ac8cd6aca869f1ef1f8cb4b6aa9225bc4a00
Lakshay989 <lakshaykucheriya@gmail.com>
2023-08-03 17:01:31 -0600

    Fixed the circular displays

--- Data_Visualization/map.js
+++ Data_Visualization/map.js
@@ -9,6 +9,23 @@ const projection = d3.geoMercator().scale(140).translate([width/2, height/1.6]) 
 const path = d3.geoPath(projection) ;
 
 const g = svg.append('g') ;
+const circle_grp = svg.append('g');
+var circleNA = circle_grp.append("circle")
+                .attr("cx", 280)
+                .attr("cy", 255)
+                .attr("r", 100).attr("fill", "red").attr("stroke" , "black").attr("stroke-width","3").attr("fill-opacity" , .35);
+
+var circleEU = circle_grp.append("circle")
+                .attr("cx", 610)
+                .attr("cy", 230)
+                .attr("r", 80).attr("fill", "blue").attr("stroke" , "black").attr("stroke-width","3").attr("fill-opacity" , .35);
+
+var circleJP = circle_grp.append("circle")
+                .attr("cx", 890)
+                .attr("cy", 310)
+                .attr("r", 25).attr("fill", "green").attr("stroke" , "black").attr("stroke-width","3").attr("fill-opacity" , .35);
+
+
 
 d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2.0.2/countries-110m.json')
         .then(data => 
